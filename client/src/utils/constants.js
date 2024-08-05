8569fea8a5b722ae1a7593634d5598b054caf622
ethdev279 <45661693+ethdev279@users.noreply.github.com>
2024-08-05 10:26:50 +0530

    added: paymaster deployment

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -3,7 +3,7 @@ import { gql } from "graphql-request";
 export const BITES_CONTRACT_ADDRESS =
   "0x8e1f23171375BC3f2DfF19f8F0F1f4a93451CB74";
 
-export const PAYMASTER_ADDRESS = "";
+export const PAYMASTER_ADDRESS = "0x0De12bb08A3c3006228269211F2505bCB444a4BA";
 
 export const GET_BITES_QUERY = gql`
   query bites(
