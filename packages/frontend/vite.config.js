3de48c7b71f83efe636563929ad78623fcf5c58d
ethdev279 <45661693+ethdev279@users.noreply.github.com>
2025-05-07 20:38:15 +0530

    updated: vite polyfils

--- packages/frontend/vite.config.js
+++ packages/frontend/vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-// import { nodePolyfills } from "vite-plugin-node-polyfills";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  define: {
+    global: {}
+  },
+  plugins: [react(), nodePolyfills({ protocolImports: true })]
 });
